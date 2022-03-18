b417134b97ae8b95181508cb9fbfac1492057223
Rick Jonkman <arjonkman@outlook.com>
2022-03-18 11:23:31 +0100

    Quick React conversion

---
+++ website/ettudo/src/Components/Body.jsx
@@ -0,0 +1,14 @@
+function Body() {
+	return (
+		<section className="Body mx-4 mt-2">
+			<div className="row">
+				<div className="col-md-6">
+					<canvas id="chart">
+					</canvas>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Body
