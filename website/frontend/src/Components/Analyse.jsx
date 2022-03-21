b6a704333cc09bcb6e5e516e50b08014043fc086
Rick Jonkman <arjonkman@outlook.com>
2022-03-21 21:21:21 +0100

    Start using ReCharts

--- website/frontend/src/Components/Analyse.jsx
+++ website/frontend/src/Components/Analyse.jsx
@@ -1,35 +1,64 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import { Row, Col, Card } from "react-bootstrap";
-
+import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area } from "recharts";
 
 function Analyse() {
-    const [temp, setTemp] = useState([]);
+  const [temp, setTemp] = useState([]);
 
-    useEffect(() => {
-        fetch(
-            "http://localhost:5000/api/temperature")
-			.then((res) => res.json())
-			.then((json) => {
-				setTemp(json)
-			}
-        );
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/temperature")
+      .then((res) => res.json())
+      .then((json) => {
+        setTemp(json);
+      });
+  }, []);
 
-    return (
-        <Row className="mx-0 p-4 justify-content-center text-center vh-100">
-            <Col xs={6}>
-                <Card>
-                    <Card.Body>
-                        <Card.Title>Analyse</Card.Title>
-                        <Card.Text>Get all of your needed hardware information from this page</Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col>
-                <p>{temp['22-03-21-17-35']}</p>
-            </Col>
-        </Row>
-    );
+  return (
+    <Row className="mx-0 p-4 justify-content-center text-center vh-100">
+      <Col xs={6}>
+        <Card>
+          <Card.Body>
+            <Card.Title>Analyse</Card.Title>
+            <Card.Text>
+              Get all of your needed hardware information from this page
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <AreaChart width={700} height={500} data={temp}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="uv"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="pv"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </Col>
+    </Row>
+  );
 }
 
 export default Analyse;
