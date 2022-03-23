e85af950b84bfe5607ab259cfc19d48a3bba1bd2
Rick Jonkman <arjonkman@outlook.com>
2022-03-23 13:47:33 +0100

    format

--- website/frontend/src/Components/Analyse.jsx
+++ website/frontend/src/Components/Analyse.jsx
@@ -1,12 +1,32 @@
 import { useState, useEffect } from "react";
 import { Row, Col, Spinner } from "react-bootstrap";
-import { ResponsiveContainer, AreaChart, XAxis, YAxis, Area, Tooltip, CartesianGrid} from "recharts";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
 
 function Analyse() {
   const [temp, setTemp] = useState([]);
 
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  let dataPoints = 0;
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
   useEffect(() => {
     fetch("http://localhost:5000/api/humidity")
@@ -19,49 +39,61 @@ function Analyse() {
   return (
     <Row className="mx-0 p-4 justify-content-center text-center vh-100">
       <Col>
-        {temp != "" ? <ResponsiveContainer width="100%" height={400}>
-          <AreaChart data={temp[1]}>
-            <Area dataKey="value" type="natural"/>
-            <XAxis dataKey="date" axisLine={false} tickLine={false} minTickGap={30} tickFormatter={str => {
-              const month = str.substring(0, 2);
-              const day = str.substring(3, 5);
-              const date = new Date(0, month, day);
-              dataPoints += 1;
-              console.log(dataPoints);
-              if (date.getDate() % 7 == 0) {
-                return months[date.getMonth()-1] + ", " + date.getDate();
-              }
-              return "";
-            }}/>
-            <YAxis dataKey="value" axisLine={false} tickCount={10} tickLine={false} domain={[0, 3500]}/>
-            <CartesianGrid vertical={false} opacity={0.2} />
-            <Tooltip content={<DataTooltip/>}/>
-          </AreaChart>
-        </ResponsiveContainer> : <Spinner animation="border" />}
+        {temp != "" ? (
+          <ResponsiveContainer width="100%" height={400}>
+            <AreaChart data={temp[1]}>
+              <Area dataKey="value" type="natural" />
+              <XAxis
+                dataKey="date"
+                axisLine={false}
+                tickLine={false}
+                minTickGap={30}
+                tickFormatter={(str) => {
+                  const month = str.substring(0, 2);
+                  const day = str.substring(3, 5);
+                  const date = new Date(0, month, day);
+                  if (date.getDate() % 7 == 0) {
+                    return months[date.getMonth() - 1] + ", " + date.getDate();
+                  }
+                  return "";
+                }}
+              />
+              <YAxis
+                dataKey="value"
+                axisLine={false}
+                tickCount={10}
+                tickLine={false}
+                domain={[0, 3500]}
+              />
+              <CartesianGrid vertical={false} opacity={0.2} />
+              <Tooltip content={<DataTooltip />} />
+            </AreaChart>
+          </ResponsiveContainer>
+        ) : (
+          <Spinner animation="border" />
+        )}
       </Col>
     </Row>
   );
 }
 
-function DataTooltip({active, payload, label}) {
+function DataTooltip({ active, payload, label }) {
   const tooltip = {
-    borderRadius: '0.25rem',
-    background: '#26313c',
-    color: '#fff',
-    padding: '1rem',
-    boxShadow: '15px 30px 40px 5px rgba(0, 0, 0, 0.5)',
-    textAlign: 'center'
+    borderRadius: "0.25rem",
+    background: "#26313c",
+    color: "#fff",
+    padding: "1rem",
+    boxShadow: "15px 30px 40px 5px rgba(0, 0, 0, 0.5)",
+    textAlign: "center",
   };
 
   if (active) {
     return (
       <div style={tooltip}>
         <h6>{label}</h6>
-        <p>
-          {payload[0].value} ppm
-        </p>
+        <p>{payload[0].value} ppm</p>
       </div>
-    )
+    );
   }
   return null;
 }
