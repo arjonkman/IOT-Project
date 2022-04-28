3171b34467e241a00fcbb45443f2f28f93898917
Rick Jonkman <arjonkman@outlook.com>
2022-04-28 17:04:48 +0200

    Added start of Rooms page and Rooms API

--- website/nextjs/my-app/pages/index.js
+++ website/nextjs/my-app/pages/index.js
@@ -1,9 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Row, Col, Card } from 'react-bootstrap'
+import Head from 'next/Head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ettudo - Home</title>
+      </Head>
+      <Row className="mx-0 p-4 justify-content-center text-center vh-100">
+        <Col xs={6} className="my-auto">
+          <Card>
+            <Card.Body>
+              <Card.Title>Ettudo</Card.Title>
+              <Card.Text>Currently under development</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
---
+++ website/nextjs/my-app/pages/rooms/index.js
@@ -0,0 +1,15 @@
+import { Row } from 'react-bootstrap'
+import Head from 'next/Head'
+
+export default function Rooms(props) {
+  return (
+    <>
+      <Head>
+        <title>Ettudo - Rooms</title>
+      </Head>
+      <Row className="mx-0 p-4 justify-content-center text-center">
+        { }
+      </Row>
+    </>
+  )
+}
