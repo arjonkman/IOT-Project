6d55cdf53b82610a2a35411e2a1cc690925d5ce2
Rick Jonkman <arjonkman@outlook.com>
2022-05-10 19:42:05 +0200

    Setup basic new layout

--- website/nextjs/my-app/pages/rooms/index.js
+++ website/nextjs/my-app/pages/rooms/index.js
@@ -1,7 +1,8 @@
-import { Row } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import Head from 'next/Head'
 
 import Room from '../../components/Room'
+import RoomSearch from '../../components/RoomSearch'
 
 export default function Rooms(props) {
   return (
@@ -9,13 +10,20 @@ export default function Rooms(props) {
       <Head>
         <title>Ettudo - Rooms</title>
       </Head>
-      <Row className="mx-0 p-4 justify-content-center text-center">
-        <Room title='Room 1' description='Room 1 light' />
-        <Room title='Room 2' description='Room 1 light' />
-        <Room title='Room 3' description='Room 1 light' />
-        <Room title='Room 4' description='Room 1 light' />
-        <Room title='Room 5' description='Room 1 light' />
-        <Room title='Room 6' description='Room 1 light' />
+      <Row className='pt-3 px-3'>
+        <Col sm={6} className='pb-3'>
+          <RoomSearch />
+        </Col>
+        <Col sm={6}>
+          <Row className="mx-0 justify-content-center text-center">
+            <Room title='Room 1' description='Room 1 light' />
+            <Room title='Room 2' description='Room 1 light' />
+            <Room title='Room 3' description='Room 1 light' />
+            <Room title='Room 4' description='Room 1 light' />
+            <Room title='Room 5' description='Room 1 light' />
+            <Room title='Room 6' description='Room 1 light' />
+          </Row>
+        </Col>
       </Row>
     </>
   )
