8ac947910cbf4ddc03d8b6c871b89661c489b38b
Francois <francoisdiel@hotmail.com>
2022-06-16 21:50:01 +0200

    Auto stash before merge of "main" and "origin/main"

--- website/nextjs/components/Tips.js
+++ website/nextjs/components/Tips.js
@@ -6,31 +6,74 @@ import axios from 'axios';
 export default function Tips() {
 	const [cookies, setCookie, removeCookie] = useCookies(['session_id']);
 	const [rooms, setRooms] = useState([]);
-	const [deets, setDeets] = useState([]);
-	const data = {};
-	const tips = {};
-	useEffect(() => {
+    const [deets, setDeets] = useState([]);
+    const tips = [];
+    useEffect(() => {
 		fetch(
 			`http://localhost:2053/api?function=GET_ROOMS&session_id=${cookies['session_id']}&room=all`
 		)
 			.then((res) => res.json())
 			.then((data) => setRooms(data));
-	}, []);
-	useEffect(() => {
-		for (let i = 0; i < rooms.length; i++) {
-			fetch(
-				`http://localhost:2053/api?function=DATA&session_id=${cookies['session_id']}&id=${rooms[i][0]}`
-			)
-				.then((res) => res.json())
-				.then((data) => {
-					setDeets(data);
-				});
-		}
-	}, [rooms, deets]);
+        
+        }, []);
+    
+    useEffect(() => {
+        for (let i = 0; i < rooms.length; i++) {
+            fetch(
+                `http://localhost:2053/api?function=LATEST_DATA&session_id=${cookies['session_id']}&id=${rooms[i][0]}`
+            )
+                .then((response) => response.json())
+                .then((data) => setDeets(data));
+                ;
+        }
+    }, [rooms]);
 
-	return (
-		<Col>
-			{/* <Card
+    function getTips() {
+        for (let i = 0; i < rooms.length; i++) {
+
+            if (deets == [])
+                break;
+
+            else {
+                let title = rooms[i][1];
+
+                for (let j = 0; j < deets.length; j++) {
+
+                    if (deets[j].type == "light") {
+                        // if (deets[j].data == 0) {
+                        //     tips[title] = "It's dark in here, you should turn on the lights!";
+                        // }
+                    }
+                    else if (deets[j].type == "temperature") {
+                        if (deets[j].data > 25) {
+                            tips[title] = "It's too hot in here, you should turn down the temperature!";
+                        }
+                        else if (deets[j].data < 16) {
+                            tips[title] = "It's too cold in here, you should turn up the temperature!";
+                        }
+                    }
+                    else if (deets[j].type == "humidity") {
+                        if (deets[j].data > 80) {
+                            tips[title] = "It's too humid in here, you should turn down the humidity!";
+                        }
+                        else if (deets[j].data < 20) {
+                            tips[title] = "It's too dry in here, you should turn up the humidity!";
+                        }
+                                               
+                    }
+                }
+
+            }
+        }
+    }
+
+    getTips();
+    console.log(tips);
+    
+
+    return (
+        <Col>
+            <Card
                 style={{
                     minHeight: '70vh',
                     borderRadius: '15px',
@@ -58,7 +101,7 @@ export default function Tips() {
                         </div>
                     ))}
                 </Card.Body>
-            </Card> */}
+            </Card>
 		</Col>
 	);
 }
--- website/nextjs/pages/index.js
+++ website/nextjs/pages/index.js
@@ -4,33 +4,7 @@ import Search from '../components/Search';
 import Map from '../components/Map';
 
 import { useCookies } from 'react-cookie';
-
-const tips = [
-	{
-		title: 'Tips 1',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat.',
-		link: '#',
-	},
-	{
-		title: 'Tips 2',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat.',
-		link: '#',
-	},
-	{
-		title: 'Tips 3',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat.',
-		link: '#',
-	},
-	{
-		title: 'Tips 4',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat.',
-		link: '#',
-	},
-];
+import Tips from '../components/Tips';
 
 export default function Home() {
 	return (
@@ -39,37 +13,7 @@ export default function Home() {
 				<title>Ettudo - Home</title>
 			</Head>
 			<Row className="mx-0 p-4 justify-content-center text-center">
-				<Col>
-					<Card
-						style={{
-							minHeight: '70vh',
-							borderRadius: '15px',
-						}}
-					>
-						<Card.Body>
-							<Card.Title>
-								<b style={{ fontSize: '3rem' }}>Tips</b>
-							</Card.Title>
-							{tips.map((tip, index) => (
-								<div key={index}>
-									<br />
-									<Card>
-										<Card.Body>
-											<Card.Title
-												style={{ fontSize: '1rem' }}
-											>
-												{tip.title}
-											</Card.Title>
-											<Card.Text>
-												{tip.description}
-											</Card.Text>
-										</Card.Body>
-									</Card>
-								</div>
-							))}
-						</Card.Body>
-					</Card>
-				</Col>
+				<Tips />
 				<Col xs={8}>
 					<b style={{ fontSize: '3rem' }}>Ruimtes</b>
 					<Search />
